5a0ed730ffc4c8356c2b3de276cc214aa7d819c9
Pastilhas <joao.campos1221@gmail.com>
2024-02-14 23:25:41 +0000

    avl: test tree

--- src/main.v
+++ src/main.v
@@ -5,31 +5,22 @@ import rand
 import time
 
 fn main() {
-	mut t := avl.new[int](compare)
-	mut a := []int{cap: 1_000_000}
-	mut c := []int{cap: 256}
+	items := int(1e7)
+	mut t := avl.new[f64](compare)
+	mut a := []f64{cap: items}
 
-	for i := 0; i < 1_000_000; i++ {
-		n := rand.u8()
-		a << n
-
-		if n !in c {
-			c << n
-		}
+	for i := 0; i < items; i++ {
+		a << rand.f64()
 	}
 
-	println('start')
 	sw := time.new_stopwatch()
-	for i := 0; i < 1_000_000; i++ {
+	for i := 0; i < items; i++ {
 		t.insert(unsafe { &a[i] })
 	}
-	println('inserted in ${sw.elapsed()}')
-
-	b := t.to_array()
-	assert b.len == c.len
+	println('${sw.elapsed()}')
 }
 
-fn compare(a int, b int) int {
+fn compare(a f64, b f64) int {
 	return if a < b {
 		-1
 	} else if a > b {
